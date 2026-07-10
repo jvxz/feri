aa6dfbc342f61ed9cf5379378210d903e0c6e4de
Jamie <hi@jamie.to>
2026-07-09 22:12:44 -0400

    chore: add accounts table + add relations

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,8 +1,26 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
+import { integer, pgEnum, pgTable, timestamp, unique, varchar } from 'drizzle-orm/pg-core'
+
+export const oauthProviderEnum = pgEnum('oauth_provider', ['discord', 'github', 'google'])
 
 export const usersTable = pgTable('users', {
-  age: integer().notNull(),
-  email: varchar().notNull().unique(),
+  avatarUrl: varchar(),
+  createdAt: timestamp().notNull().defaultNow(),
+  displayName: varchar().notNull(),
+  email: varchar().unique(),
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar().notNull(),
+  username: varchar().notNull().unique(),
 })
+
+export const accountsTable = pgTable(
+  'accounts',
+  {
+    createdAt: timestamp().notNull().defaultNow(),
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    provider: oauthProviderEnum().notNull(),
+    providerAccountId: varchar().notNull(),
+    userId: integer()
+      .notNull()
+      .references(() => usersTable.id, { onDelete: 'cascade' }),
+  },
+  table => [unique().on(table.provider, table.providerAccountId)],
+)
